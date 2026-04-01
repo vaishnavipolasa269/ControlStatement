c2c8fc6fda7e9a5f3727833d8e15083941185fa1
vaishnavipolasa269 <vaishnavipolasa269@gmail.com>
2026-04-01 19:29:00 +0530

    Add files via upload

---
+++ Controlstatements/Task2.java
@@ -0,0 +1,13 @@
+package com.Controlstatements;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10;i>=1;i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ Controlstatements/Task5.java
@@ -0,0 +1,12 @@
+package com.Controlstatements;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=5;i<=50;i+=5) {
+	System.out.println(i);
+}
+	}
+
+}
